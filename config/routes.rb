2c0ffacb1bb2cd79e6aff878e682630b98b76d51
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2018-11-25 16:40:12 -0200

    create models user and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  
+  get '/sobre', to: 'pages#sobre', as: 'sobre'
+  get '/contato', to: 'pages#contato', as: 'contato'
+
   resources :contacts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_180602) do
+ActiveRecord::Schema.define(version: 2018_11_25_181318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_11_25_180602) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
